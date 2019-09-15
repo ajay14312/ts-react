b48eedb88bd3981c046645b90df5fc32234c25dc
ajaym <ajaym@marvell.com>
2019-09-15 18:41:11 +0530

    Updates
    Change-Id: Ibf39ff7ffee86e649982bff05c3853767c8a815d

---
+++ precache-manifest.f72f16d487aa02a49ea5917fb54b5b46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781c4a704652c537d2bec72c4768111d",
+    "url": "/ts-react/index.html"
+  },
+  {
+    "revision": "36d7ebd5bd99bf7b955a",
+    "url": "/ts-react/static/css/main.7592320d.chunk.css"
+  },
+  {
+    "revision": "2fc6d4637f69f809df32",
+    "url": "/ts-react/static/js/2.02071ad2.chunk.js"
+  },
+  {
+    "revision": "36d7ebd5bd99bf7b955a",
+    "url": "/ts-react/static/js/main.79f12b30.chunk.js"
+  },
+  {
+    "revision": "afb17b9afb28a7a480fd",
+    "url": "/ts-react/static/js/runtime~main.8a403d85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ts-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5771d4e76e2c701bd1dbad5d00388b90.js"
+  "/ts-react/precache-manifest.f72f16d487aa02a49ea5917fb54b5b46.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ts-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
